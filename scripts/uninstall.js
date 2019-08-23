d47b86c1ef296b3d8498b169d743ac14f5370c40
tnarik <tnarik@lecafeautomatique.co.uk>
2019-08-23 21:25:10 +0100

    Linting

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -3,9 +3,10 @@ const path = require('path')
 const fs = require('fs')
 
 nm_host = require(path.join(path.dirname(__dirname), "nm_host.json"))
+package = require(path.join(path.dirname(__dirname), "package.json"))
 
 // Native Messaging manifest file destination
-const nm_mnf_file = `${nm_host.name}.json`
+const nm_mnf_file = `${package.nativeMessaging.name}.json`
 const nm_mnf_folder = path.join(require('os').homedir(), "Library", "Application Support", "Google", "Chrome", "NativeMessagingHosts")
 
 fs.unlink(path.join(nm_mnf_folder, nm_mnf_file), err => {
